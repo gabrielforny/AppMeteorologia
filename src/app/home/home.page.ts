af3db77a97dc8995ac5f81b0e681ac63bbd64c2e
Gabriel Forny <gf097@hotmail.com>
2023-08-05 02:38:21 -0300

    feature: primeira parte da aplicação

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface infoDataCities {
+  name: string;
+  state: string;
+  id: number;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,7 +13,88 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  apiKey = '8b9cb2497394a2f8519f3f76b9d217eb';
+  city = 'RJ';
+  country = 'BR';
 
-  constructor() {}
+  resultPrevision: infoDataCities[] = [];
+  searchFilter: infoDataCities[] = [];
+  resultPagination: infoDataCities[] = [];
+  uniqueStates: any[]=[];
 
+  currentPage = 1;
+  itemsPerPage = 20;
+  totalPages = 1;
+
+  filterCity = '';
+  filterState = '';
+
+  darkModeEnabled: boolean = false;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getStatesForCountry();
+  }
+
+  getStatesForCountry() {
+    const url = `http://apiadvisor.climatempo.com.br/api/v1/locale/city?country=${this.country}&token=${this.apiKey}`;
+    this.http.get<infoDataCities[]>(url).subscribe((data) => {
+      this.resultPrevision = data;
+      this.searchFilter = data;
+      this.resultPagination = this.resultPrevision.slice(0, this.itemsPerPage);
+      this.totalPages = Math.ceil(this.resultPrevision.length / this.itemsPerPage);
+
+      this.getUniqueStates();
+    });
+  }
+
+  getUniqueStates() {
+    const states = new Set<string>();
+    this.resultPrevision.forEach((data) => {
+      states.add(data.state);
+    });
+    this.uniqueStates = Array.from(states);
+  }
+
+  applyFilter() {
+    let filteredData = this.resultPrevision;
+
+    if (this.filterCity) {
+      filteredData = filteredData.filter(data =>
+        data.name.toLowerCase().includes(this.filterCity.toLowerCase())
+      );
+    }
+
+    if (this.filterState) {
+      filteredData = filteredData.filter(data =>
+        data.state.toLowerCase() === this.filterState.toLowerCase()
+      );
+    }
+
+    this.searchFilter = filteredData;
+    this.resultPagination = this.searchFilter.slice(0, this.itemsPerPage);
+    this.currentPage = 1;
+    this.totalPages = Math.ceil(this.searchFilter.length / this.itemsPerPage);
+  }
+
+  pageLoading(direction: number) {
+    const newPage = this.currentPage + direction;
+    if (newPage >= 1 && newPage <= this.totalPages) {
+      const startIndex = (newPage - 1) * this.itemsPerPage;
+      const endIndex = startIndex + this.itemsPerPage;
+      this.resultPagination = this.searchFilter.slice(startIndex, endIndex);
+      this.currentPage = newPage;
+    }
+  }
+
+  getTemperatureImage(temperature: number): string {
+    if (temperature >= 30) {
+      return 'assets/images/sunny.png';
+    } else if (temperature >= 20) {
+      return 'assets/images/partly_sunny.png';
+    } else {
+      return 'assets/images/cloudy.png';
+    }
+  }
 }
